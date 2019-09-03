073dac9336abcc25f3c24a595061cced282c0d57
Nick van Bakel <nick.vb@endouble.com>
2019-09-03 10:30:07 +0200

    Added Amsterdam monuments

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from api.amsterdam_api import AmsterdamApi
 def main():
     amsterdam_api = AmsterdamApi()
     list_trash_bins = amsterdam_api.get_trash_bins()
+    list_monuments = amsterdam_api.get_monuments()
 
-    print("Overview of trash bins in Amsterdam")
+    print("Overview of trash bins in Amsterdam \n")
 
     for trash_bin in list_trash_bins:
         print(
@@ -13,6 +14,14 @@ def main():
             trash_bin['name'] + "\t" +
             trash_bin['type'] + "\t" +
             trash_bin['address']
+        )
+
+    print("Overview of monuments in Amsterdam \n")
+
+    for monument in list_monuments:
+        print(
+            str(monument['id']) + "\t" +
+            monument['address'] + "\t"
         )
 
 
